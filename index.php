8061d472c3277340505782b3a5af27ec3281f49a
Leandro-Trinidad-QECS <l.trinidad@share.epsb.ca>
2017-10-11 10:52:30 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+
+<head>
+	<title>Full Screen Landing Page</title>
+	<meta name="viewport"
+	  content="width=device-width, initial-scale=1">
+	<link rel="stylesheet"
+	  type="text/css"
+	  href="style.css">
+	<link rel="stylesheet"
+	  href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+	<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+	<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+	<link href="css/animate.css"
+	  rel="stylesheet" />
+	<link href="css/waypoints.css"
+	  rel="stylesheet" />
+	<script src="js/jquery.waypoints.min.js"
+	  type="text/javascript"></script>
+	<script src="js/waypoints.js"
+	  type="text/javascript"></script>
+</head>
+
+<body>
+	<section class="os-animation"
+	  data-os-animation="fadeInDown"
+	  data-os-animation-delay="0s">
+		<nav>
+			<div class="navigation-bar container">
+				<a href="#">
+					pager
+				</a>
+			</div>
+		</nav>
+	</section>
+
+
+	<div class="container">
+		<form>
+			<section class="intro">
+				<div class="intro">
+					<div class="content">
+						<section class="os-animation"
+						  data-os-animation="fadeInUp"
+						  data-os-animation-delay=".5s">
+							<form action="welcome.php"
+							  method="post">
+								<input class="textInput"
+								  type="text"
+								  name="name"
+								  placeholder="share your thoughts">
+								<input type="submit"
+								  value="post"
+								  class="button button1">
+							</form>
+							<!-- TODO: Make a form that will work for php -->
+						</section>
+					</div>
+				</div>
+			</section>
+
+		</form>
+	</div>
+
+
+</body>
+
+</html>
